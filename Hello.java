bf6046a932c73b388078b4db3e111576eacb557a
btoolAlotaibi <114299172+btoolAlotaibi@users.noreply.github.com>
2022-09-24 19:28:00 +0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello{
+    public static void main (String[]args){
+       System.out.println("Hello Programmers");
+       System.out.println("Welcome to the course");
+      //System.out.print("Learn\tJava\n\tthe\nHard\tWay\n\n");
+      //System.out.print("\tLearnjavathe\"Hard\"Way!\n");
+      //System.out.print("Hello\n");
+      //System.out.print("jellow\rY\n");
+     // System.out.println("\\ // -=- \\ //");
+     // System.out.println("\\\\ \\\\\\ \\\\\\\\");
+     //System.out.printf("%n%d%n%f%n%b%n%c%n%",7,3.14,true,'a',"hello");
+    }
+}
